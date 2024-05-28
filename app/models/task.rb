d77f796b70ec89e9c3874c887ad0f635109f4021
gabrielgiacometti <henrique.giacometti@aluno.ifsp.edu.br>
2024-05-28 17:31:15 -0300

    add turbo to index

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-    has_many :sub_task, foreign_key: "main_task_id", class_name:"Task",dependent: :delete_all
+    has_many :sub_task, foreign_key: "main_task_id", class_name:"Task", dependent: :delete_all
     belongs_to :main_task, class_name: "Task", optional: true
 end
