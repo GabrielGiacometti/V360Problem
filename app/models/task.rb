b80738a046037c5e6ac84ec7966cbb1a538819b4
gabrielgiacometti <henrique.giacometti@aluno.ifsp.edu.br>
2024-05-29 01:49:07 -0300

    fix destroy all

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
     scope :ordered, ->{order(created_at: :desc)}
-    has_many :sub_task, foreign_key: "main_task_id", class_name:"Task", dependent: :delete_all
+    has_many :sub_task, foreign_key: "main_task_id", class_name:"Task", dependent: :destroy
     belongs_to :main_task, class_name: "Task", optional: true
     validates :title, presence:true
 end
